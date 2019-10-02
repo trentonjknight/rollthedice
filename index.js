d733c5bcd4477cb309e08a91383cc05906219c13
audioryan9 <ryandsgn@outlook.com>
2019-10-02 20:16:49 +0000

    sdfsdf

--- index.js
+++ index.js
@@ -1,37 +1,42 @@
-const getVal = (number)
-const getDice = (end) => {
-    const getRandomDice = () => ({
-        number: Math.floor(Math.random() * 6) + 1,
-    });
-    let dice = [];
-    for (let i = 0; i < end; i++) dice.push(getRandomDice());
-    return dice;
+function getRandom(){
+
+    let xdice = Math.floor(Math.random()*6)+1;
+    let twopac = Math.floor(Math.random()*6)+1;
+
+    document.querySelector('.cube').innerHTML= `
+        <div class="front side"><h1>${xdice}</h1></div>
+        <div class="back side"><h1>${xdice}</h1></div>
+        <div class="left side"><h1>${xdice}</h1></div>
+        <div class="right side"><h1>${xdice}</h1></div>`
+
+
+    document.querySelector('.unsortedDice').innerHTML= `
+        <div class="wrap">
+        <div class="cube">
+        <div class="front side"><h1>${Math.floor(Math.random()*6)+1}</h1></div>
+        <div class="back side"><h1>${Math.floor(Math.random()*6)+1}</h1></div>
+        <div class="left side"><h1>${Math.floor(Math.random()*6)+1}</h1></div>
+        <div class="right side"><h1>${Math.floor(Math.random()*6)+1}</h1></div>
+        </div>
+        </div>`
 }
 
-var log = [];
-let dice = [];
-const selection = (ryan) => {
-   let min = 0;
-   while (min < ryan.length-1){
-       for(let i = min+1; i < ryan.length-1; i++) {
-         if (ryan[min].num > ryan[i].num) {
-           let store = ryan[min]; ryan[min] = ryan[i]; ryan[i] = store;
-           log.push(ryan.slice(0));
-         }
-       }
-       min++;
-   }
-        return ryan;
-};
+function getDice() {
+    let xdice = Math.floor(Math.random()*6)+1;
+//    return `<div><h1>${xdice}</h1></div>`
+    return `
+        <div class="wrap">
+            <div class="cube">
+                <div class="front side"><h1>${xdice}</h1></div>
+                <div class="back side"><h1>${xdice}</h1></div>
+                <div class="left side"><h1>${xdice}</h1></div>
+                <div class="right side"><h1>${xdice}</h1></div>
+            </div>
+        </div>`
 
-const showMeTheCube = (cube) => `<div class="cube"><span>${getVal(cube.num)}</span></div>`;
+}
 
-document.querySelector('#roll').addEventListener('click', () => {
-    dice = getDice(document.querySelector('#diceAmount').value);
- document.querySelector('.unsortedDice').innerHTML = dice.map(cube => showMeTheCube(cube)).join('');
- document.querySelector('.sortPath').innerHTML = '';
-});
-document.querySelector('#sort').addEventListener('click', () => {
- selection(dice);
- document.querySelector('.sortPath').innerHTML = log.map((iter,path) => `<li><i>${path}</i><div class="cube">${iter.map(cube => showMeTheCube(cube)).join('')}</div></li>`).join('');
-});
+function rollDice() {
+    document.querySelector('.unsortedDice').innerHTML = getDice() + getDice() + getDice() + getDice() + getDice();
+    //document.querySelector('.unsortedDice').innerHTML = `${getDice()}${getDice()}${getDice()}${getDice()}${getDice()}`;
+}
