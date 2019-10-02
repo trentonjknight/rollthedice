8101817800939f64e30c86e58dfb6d5ded0f0e9f
Trenton Knight <tknightj1995@yahoo.com>
2019-10-02 20:52:55 +0000

    cleanup

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function getRandom(){
 
-    let xdice = Math.floor(Math.random()*6)+1;
-    let twopac = Math.floor(Math.random()*6)+1;
+    let dice = Math.floor(Math.random()*6)+1;
+
 
     document.querySelector('.cube').innerHTML= `
-        <div class="front side"><h1>${xdice}</h1></div>
-        <div class="back side"><h1>${xdice}</h1></div>
-        <div class="left side"><h1>${xdice}</h1></div>
-        <div class="right side"><h1>${xdice}</h1></div>`
+        <div class="front side"><h1>${dice}</h1></div>
+        <div class="back side"><h1>${dice}</h1></div>
+        <div class="left side"><h1>${dice}</h1></div>
+        <div class="right side"><h1>${dice}</h1></div>`
 
 
     document.querySelector('.unsortedDice').innerHTML= `
@@ -22,15 +22,15 @@ function getRandom(){
 }
 
 function getDice() {
-    let xdice = Math.floor(Math.random()*6)+1;
+    let dice = Math.floor(Math.random()*6)+1;
 //    return `<div><h1>${xdice}</h1></div>`
     return `
         <div class="wrap">
             <div class="cube">
-                <div class="front side"><h1>${xdice}</h1></div>
-                <div class="back side"><h1>${xdice}</h1></div>
-                <div class="left side"><h1>${xdice}</h1></div>
-                <div class="right side"><h1>${xdice}</h1></div>
+                <div class="front side"><h1>${dice}</h1></div>
+                <div class="back side"><h1>${dice}</h1></div>
+                <div class="left side"><h1>${dice}</h1></div>
+                <div class="right side"><h1>${dice}</h1></div>
             </div>
         </div>`
 
@@ -38,5 +38,4 @@ function getDice() {
 
 function rollDice() {
     document.querySelector('.unsortedDice').innerHTML = getDice() + getDice() + getDice() + getDice() + getDice();
-    //document.querySelector('.unsortedDice').innerHTML = `${getDice()}${getDice()}${getDice()}${getDice()}${getDice()}`;
 }
